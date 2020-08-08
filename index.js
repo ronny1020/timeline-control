9a43d64f96df63d01d92d9a945600b83f83ebb86
Ronny Hsieh <1020ronny@gmail.com>
2020-08-08 10:27:03 +0800

    basic settimeout promise

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('test')
+
+function countTime(action, time) {
+  return new Promise(function (resolve) {
+    action()
+    setTimeout(resolve, time)
+  })
+}
+
+async function runTimeline() {
+  for (let i = 0; i < 10; i++) {
+    await countTime(() => console.log(i), 1000)
+  }
+}
+
+runTimeline()
